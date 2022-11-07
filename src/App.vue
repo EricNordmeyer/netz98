bf5a6c6e13888028ba5a4cf2872e69e634dee7c2
Eric Nordmeyer <eric.nordmeyer@gastro-hero.de>
2022-11-07 23:03:13 +0100

    feat: add first iteration of netz98 rss reader

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { onMounted, ref, reactive } from "@vue/runtime-core"
+import { XMLParser } from 'fast-xml-parser'
+import FeedItem from './components/FeedItem.vue'
+
+const state = reactive({ feedItems: [], title: '', description: '', channel: {} })
+
+onMounted(() => {
+  getRss()
+})
+
+function getRss() {
+    try {
+      const parser = new XMLParser();
+      fetch('http://localhost:1234/feed/')
+      .then(async (response) => {
+        let result = await response.text();
+        let rssFeed = parser.parse(result)
+        state.channel = rssFeed.rss.channel
+      })
+    }
+    catch {
+      console.log(err)
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1><a v-bind:href="state.channel.link" class="green"> {{ state.channel.title }}</a>{{ state.channel.description }}</h1>
+    </header>
+    <div v-for="item, index in state.channel.item" :key="index">
+      <FeedItem>
+        <template #heading><a :href="item.link">{{ item.title }}</a></template>
+        <template #description><p v-html="item.description" /></template>
+        <div v-html="item['content:encoded']" />
+      </FeedItem>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+  header {
+    margin-bottom: 24px;
+  }
+
+  h1 a{
+    color: #009;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+</style>
---
+++ src/components/FeedItem.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { reactive } from "@vue/runtime-core"
+
+const state = reactive({buttonText: 'mehr', showContent: false})
+
+function toggleContent() {
+  state.showContent = !state.showContent
+  state.buttonText = state.showContent ? 'weniger' : 'mehr'
+}
+</script>
+
+<template>
+  <div class="item">
+    <h2>
+      <slot name="heading"></slot>
+    </h2>
+    <p class="description"  >
+      <slot name="description"></slot>
+    </p>
+    <p class="content" v-if="state.showContent">
+      <slot></slot>
+    </p>
+    <button @click="toggleContent">{{ state.buttonText }}</button>
+  </div>
+</template>
+
+<style scoped>
+  .item {
+    border: 1px solid #999;
+    border-radius: 6px;
+    padding: 0 8px 8px;
+    margin-bottom: 12px;
+    background-color: #EEE;
+  }
+
+  h2 {
+    margin-bottom: 4px;
+    font-weight: 500;
+  }
+
+  .description {
+    margin-bottom: 20px
+  }
+
+  button {
+    background: #009;
+    color: #fff;
+  }
+</style>
